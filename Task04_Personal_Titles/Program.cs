a2319a67973f0769a16dc95c97a98eb3ec43c460
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-06 20:03:55 +0200

    Add files via upload

---
+++ Task04_Personal_Titles/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task04_Personal_Titles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+            // check and output
+            switch (gender)
+            {
+                case "m":
+                    if (age >= 16)
+                    {
+                        Console.WriteLine("Mr.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Master");
+                    }
+                    break;
+                case "f":
+                    if (age >= 16)
+                    {
+                        Console.WriteLine("Ms.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Miss");
+                    }
+                    break;
+            }
+        }
+    }
+}
