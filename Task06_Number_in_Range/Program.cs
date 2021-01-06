ed1dafe8c249b86c456457fe269182fefd6bec48
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-06 20:06:04 +0200

    Add files via upload

---
+++ Task06_Number_in_Range/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Number_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int number = int.Parse(Console.ReadLine());
+            // check and output
+            if (number == 0)
+            {
+               Console.WriteLine("No");
+            }
+            else if (number < -100 | number > 100)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("Yes");
+            }
+        }
+    }
+}
