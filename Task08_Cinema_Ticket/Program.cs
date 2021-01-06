88693e56de22c7157b7e47ac6259a40081ceed7a
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-06 20:07:58 +0200

    Add files via upload

---
+++ Task08_Cinema_Ticket/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task08_Cinema_Ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            string day = Console.ReadLine();
+            // check and output
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Friday":
+                    {
+                        Console.WriteLine("12");
+                    }
+                    break;
+                case "Wednesday":
+                case "Thursday":
+                    {
+                        Console.WriteLine("14");
+                    }
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    {
+                        Console.WriteLine("16");
+                    }
+                    break;
+            }
+        }
+    }
+}
