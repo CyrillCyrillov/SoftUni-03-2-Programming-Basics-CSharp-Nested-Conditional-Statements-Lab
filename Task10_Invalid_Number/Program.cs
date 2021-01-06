6650fb98036e368a8d84adce685e53a51e705732
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-06 20:09:57 +0200

    Add files via upload

---
+++ Task10_Invalid_Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task10_Invalid_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int number = int.Parse(Console.ReadLine());
+            // check and output
+            if (number != 0)
+            {
+                if (number < 100 | number > 200)
+                {
+                    Console.WriteLine("invalid");
+                }
+            }
+        }
+    }
+}
