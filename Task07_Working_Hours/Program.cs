87763f4b698dc3309c1ef0b9b92a88390732554c
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-06 20:07:00 +0200

    Add files via upload

---
+++ Task07_Working_Hours/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Task07_Working_Hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int time = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            // check and output
+            switch (day)
+            {
+                case "Sunday":
+                    Console.WriteLine("closed");
+                    break;
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                case "Saturday":
+                    if (time >= 10 && time < 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+            }
+        }
+    }
+}
