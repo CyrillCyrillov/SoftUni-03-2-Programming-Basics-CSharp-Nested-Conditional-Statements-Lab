c52df8f06f16dd09ea011caa9503953a3d22feb3
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-06 20:09:01 +0200

    Add files via upload

---
+++ Task09_Fruit_or_Vegetable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task09_Fruit_or_Vegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            string product = Console.ReadLine();
+            // check and output
+            switch (product)
+            {
+                case "banana":
+                case "apple":
+                case "kiwi":
+                case "cherry":
+                case "lemon":
+                case "grapes":
+                    {
+                        Console.WriteLine("fruit");
+                    }
+                    break;
+                case "tomato":
+                case "cucumber":
+                case "pepper":
+                case "carrot":
+                    {
+                        Console.WriteLine("vegetable");
+                    }
+                    break;
+                default:
+                    {
+                        Console.WriteLine("unknown");
+                    }
+                    break;
+            }
+        }
+    }
+}
