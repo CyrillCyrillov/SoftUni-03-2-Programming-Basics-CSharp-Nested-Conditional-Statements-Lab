79c1819be66d968d46bd8dbf5d96c74c1ecb6ed1
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-06 20:03:05 +0200

    Add files via upload

---
+++ Task03_Animal_Type/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Таск03_Animal_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            string animal = Console.ReadLine();
+            // check and output
+            switch (animal)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
